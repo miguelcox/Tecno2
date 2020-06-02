8e597c5239fdfb660b4b56db6a2b779fe7cd7b9f
miguelcox <65303717+miguelcox@users.noreply.github.com>
2020-06-02 12:07:21 -0500

    Update index.php

--- index.php
+++ index.php
@@ -159,9 +159,7 @@
       <div class="container" data-aos="fade-in">
 
         <div class="text-center">
-          <h3>Call To Action</h3>
-          <p> Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-          <a class="cta-btn" href="#">Call To Action</a>
+          <h3>EXPERTOS EN CONTROL DE PLAGAS Y MANTENIMIENTOS PREVENTIVOS Y CORRECTIVOS CON GARANTÍA</h3>
         </div>
 
       </div>
