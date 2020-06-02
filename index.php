c6a440cc6caa069e9e4f67f43619f71d7c12b533
miguelcox <65303717+miguelcox@users.noreply.github.com>
2020-06-02 13:06:05 -0500

    Update index.php

--- index.php
+++ index.php
@@ -38,33 +38,32 @@
 
 <body>
 
-    <!-- ======= Header ======= -->
+  <!-- ======= Header ======= -->
   <header id="header" class="fixed-top">
     <div class="container d-flex">
-
       <div class="logo mr-auto">
         <h1 class="text-light"><a href="index.html">TECNOFUMIGA</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
       </div>
-
       <nav class="nav-menu d-none d-lg-block">
         <ul>
           <li class="active"><a href="index.php">Inicio</a></li>
-
           <li class="drop-down"><a href="#">¿Quienes Somos?</a>
             <ul>
               <li><a href="about.php">Nosotros</a></li>
-              <li><a href="team.php">Misión</a></li>
-              <li><a href="team.php">Visión</a></li>
-              <li><a href="team.php">Valores</a></li>
             </ul>
           </li>
           <li><a href="services.php">Servicios</a></li>
           <li><a href="contact.php">Contacto</a></li>
+              <li class="drop-down"><a href="#">Login</a>
+                <ul>
+                  <li><a class="get-started" href="#">Administrativos</a></li>
+                  <li><a class="get-started" href="#">Clientes</a></li>
+                </ul>
+              </li>
         </ul>
       </nav><!-- .nav-menu -->
-
     </div>
   </header><!-- End Header -->
 
