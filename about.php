80cc989f015db8881074ff295156a3968b0c9a77
miguelcox <65303717+miguelcox@users.noreply.github.com>
2020-06-02 13:53:38 -0500

    Update about.php

--- about.php
+++ about.php
@@ -201,15 +201,17 @@
             <img src="assets/img/work-process-2.png" class="img-fluid" alt="">
           </div>
           <div class="col-md-7 pt-5 order-2 order-md-1" data-aos="fade-right">
-            <h3>Corporis temporibus maiores provident</h3>
+            <h3>Nuestra Misión</h3>
             <p class="font-italic">
               Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore
               magna aliqua.
             </p>
             <p>
-              Ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
-              velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-              culpa qui officia deserunt mollit anim id est laborum
+              Lograr la fidelidad y satisfacción total de nuestros clientes, 
+	      a través de técnicas innovadoras de altísima calidad, tecnología y costos competitivos, 
+	      brindando apoyo y asesoría en todo momento, generando confianza y seguridad, ofreciendo
+	      la adecuada garantía que nuestros clientes exigen, cumpliendo con las normativas vigentes 
+	      y respetando siempre el medio ambiente
             </p>
           </div>
         </div>
