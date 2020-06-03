182f1ad422ebddeeddb0a485eab89fc92681453f
miguelcox <65303717+miguelcox@users.noreply.github.com>
2020-06-02 20:37:20 -0500

    Update index.php

--- index.php
+++ index.php
@@ -163,30 +163,29 @@
 
       </div>
     </section><!-- End Cta Section -->
-
+	  <br>
     <!-- ======= Features Section ======= -->
     <section id="features" class="features">
       <div class="container">
-
         <div class="section-title" data-aos="fade-up">
           <h2>Nuestros Servicios</h2>
-          <p>Magnam dolores commodi suscipit. Necessitatibus eius consequatur ex aliquid fuga eum quidem. Sit sint consectetur velit. Quisquam quos quisquam cupiditate. Et nemo qui impedit suscipit alias ea. Quia fugiat sit in iste officiis commodi quidem hic quas.</p>
+          <p>En Tecnofumiga te brindamos los siguientes servicios</p>
         </div>
 
         <div class="row">
           <div class="col-md-6 d-flex align-items-stretch" data-aos="fade-up">
-            <div class="card" style="background-image: url(assets/img/features-1.jpg);">
+            <div class="card" style="background-image: url(assets/img/fumiga.png);">
               <div class="card-body">
-                <h5 class="card-title"><a href="">Our Mission</a></h5>
+                <h5 class="card-title"><a href="">Fumigación</a></h5>
                 <p class="card-text">Lorem ipsum dolor sit amet, consectetur elit, sed do eiusmod tempor ut labore et dolore magna aliqua.</p>
                 <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
           </div>
           <div class="col-md-6 d-flex align-items-stretch mt-4 mt-md-0" data-aos="fade-up">
-            <div class="card" style="background-image: url(assets/img/features-2.jpg);">
+            <div class="card" style="background-image: url(assets/img/sanitizacion.jpg);">
               <div class="card-body">
-                <h5 class="card-title"><a href="">Our Plan</a></h5>
+                <h5 class="card-title"><a href="">Sanitización</a></h5>
                 <p class="card-text">Sed ut perspiciatis unde omnis iste natus error sit voluptatem doloremque laudantium, totam rem.</p>
                 <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
@@ -194,18 +193,18 @@
 
           </div>
           <div class="col-md-6 d-flex align-items-stretch mt-4" data-aos="fade-up" data-aos-delay="100">
-            <div class="card" style="background-image: url(assets/img/features-3.jpg);">
+            <div class="card" style="background-image: url(assets/img/plagas.jpg);">
               <div class="card-body">
-                <h5 class="card-title"><a href="">Our Vision</a></h5>
+                <h5 class="card-title"><a href="">Control de plagas</a></h5>
                 <p class="card-text">Nemo enim ipsam voluptatem quia voluptas sit aut odit aut fugit, sed quia magni dolores.</p>
                 <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
             </div>
           </div>
           <div class="col-md-6 d-flex align-items-stretch mt-4" data-aos="fade-up" data-aos-delay="100">
-            <div class="card" style="background-image: url(assets/img/features-4.jpg);">
+            <div class="card" style="background-image: url(assets/img/areasverdes.jpg);">
               <div class="card-body">
-                <h5 class="card-title"><a href="">Our Care</a></h5>
+                <h5 class="card-title"><a href="">Áreas Verdes</a></h5>
                 <p class="card-text">Nostrum eum sed et autem dolorum perspiciatis. Magni porro quisquam laudantium voluptatem.</p>
                 <div class="read-more"><a href="#"><i class="icofont-arrow-right"></i> Read More</a></div>
               </div>
